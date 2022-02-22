95171c89a207ddde1f20d9ff707f7468e2ef90b8
sizwe <nkosiczwe@gmail.com>
2022-02-22 21:20:59 +0200

    Add DOM elements

---
+++ script.js
@@ -0,0 +1,13 @@
+let equal_pressed = 0;
+//Refer all buttons excluding AC and DEL
+let button_input = document.querySelector(".input-button");
+//Refer input,equal,clear and erase
+let input = document.getElementById("input");
+let input = document.getElementById("equal");
+let input = document.getElementById("clear");
+let input = document.getElementById("erase");
+
+window.onload = () => {
+    input.value = "";
+}
+
